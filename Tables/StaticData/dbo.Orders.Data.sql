95aac6833bebd959fc74d06aaf40666c7039e0b5
DATASOFT\YuliaV <yuliav@devart.com>
2025-08-11 18:06:47 +0300

    link static data

---
+++ Tables/StaticData/dbo.Orders.Data.sql
@@ -0,0 +1,55 @@
+﻿SET IDENTITY_INSERT dbo.Orders ON
+GO
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (1, 'Catherina533', 'GD', '1972-09-16');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (2, 'Carmelina382', '5MALZ', '2012-07-23');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (3, 'Mead1954', '3H19', '1999-05-30');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (4, 'Adelaida2', '4365', '2003-03-06');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (5, NULL, NULL, NULL);
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (6, NULL, NULL, NULL);
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (7, 'Lanier4', '2D516QOH', '2005-06-29');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (8, 'Jarod2021', 'IJ', '1976-12-03');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (9, 'Abney2020', 'E', '2019-10-20');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (10, 'Rawlings1966', '01', '1981-01-15');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (11, 'Zook2014', 'V4', '1991-01-18');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (12, 'Denna1961', 'KE6J0Z5474', '1998-11-18');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (13, 'Bert631', '74CY', '1970-01-05');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (14, 'Crouch93', '72', '2012-10-21');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (15, 'Wynona1996', '80', '2007-01-25');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (16, 'Hawkins231', 'O3M6U43C9K', '1979-06-29');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (17, 'Wheeler2', '2DW', '1970-07-02');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (18, 'Lester2029', '5', '1976-08-11');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (19, 'Riva1959', 'F', '1971-07-20');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (20, 'Acevedo1972', 'S', '2023-11-03');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (21, 'Ziegler1970', 'Z9Z', '1970-09-02');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (22, 'Shaw2019', 'S', '1978-01-28');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (23, 'Alfonzo534', 'M', '2013-10-14');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (24, 'Cisneros36', '3', '1970-01-10');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (25, 'Antwan96', 'DPB', '1970-01-24');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (26, NULL, NULL, NULL);
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (27, 'Alanna8', '63', '2021-02-11');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (28, 'Jamey848', '0A8195', '1980-12-28');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (29, 'Mitchell5', 'O', '2002-06-01');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (30, 'Enrique2006', '02', '1998-01-10');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (31, 'Courtney2007', '9212W', '1996-01-04');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (32, 'Xavier1964', '2539O9OF6', '1979-07-27');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (33, 'Begay34', 'OX', '1970-03-23');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (34, 'Shanna1960', '7U', '1979-08-24');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (35, 'Angelo2018', 'CRU0V', '1971-09-20');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (36, 'Addie2014', '2SN', '2005-09-26');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (37, 'Giovanni9', '8J', '1987-11-08');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (38, 'Adair845', '36H8', '2008-10-26');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (39, 'Patricia1994', '6A7', '1975-04-09');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (40, 'Pringle1994', '97', '1982-01-18');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (41, 'Mears617', 'CY9', '2003-04-10');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (42, 'Enriquez819', 'I', '1981-02-16');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (43, 'Harley1998', '42', '1970-09-02');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (44, 'Winslow1950', '3', '1972-02-08');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (45, NULL, NULL, NULL);
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (46, 'Abraham2020', 'Y', '1987-09-12');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (47, 'Christian6', 'DQNBB3BBK3G6', '1971-06-14');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (48, 'Milliken166', 'S613O', '1992-12-11');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (49, 'Alec1986', '3', '1971-05-13');
+INSERT dbo.Orders(order_id, customer_name, status, order_date) VALUES (50, NULL, NULL, NULL);
+GO
+SET IDENTITY_INSERT dbo.Orders OFF
+GO
--- Tables/dbo.actor.sql
+++ Tables/dbo.actor.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[actor] (
   [actor_id] [int] IDENTITY,
   [first_name] [varchar](45) NOT NULL,
-  [last_name] [varchar](45) NOT NULL,
+  [last_name] [varchar](75) NOT NULL,
   [last_update] [date] NOT NULL CONSTRAINT [DF_actor_last_update] DEFAULT (getdate()),
   PRIMARY KEY CLUSTERED ([actor_id])
 )
